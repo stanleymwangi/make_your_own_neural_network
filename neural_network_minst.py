cc45eb427d3d92e84fc5a0f33631ac62315366e4
Stanley Mwangi <stanthecoolman@gmail.com>
2018-01-19 19:41:33 +0000

    Add skeleton code for neural network

---
+++ neural_network_minst.py
@@ -0,0 +1,17 @@
+import numpy as np
+from scipy import special
+
+# neural network class definition
+class NeuralNetwork:
+
+    # initialise the neural network
+    def __init__(self):
+        pass
+
+    # train the neural network
+    def train(self):
+        pass
+
+    # query the neural network
+    def query(self):
+        pass
