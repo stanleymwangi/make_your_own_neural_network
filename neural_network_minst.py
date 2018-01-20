422072990abb41932dfe30cd73296e8b75ae161e
Stanley Mwangi <stanthecoolman@gmail.com>
2018-01-20 20:55:22 +0000

    Add neural network initialisation code

--- neural_network_minst.py
+++ neural_network_minst.py
@@ -5,8 +5,16 @@ from scipy import special
 class NeuralNetwork:
 
     # initialise the neural network
-    def __init__(self):
-        pass
+    def __init__(self, input_nodes, hidden_nodes, output_nodes, learning_rate):
+        # set the number of nodes in layers
+        self.input_nodes = input_nodes
+        self.hidden_nodes = hidden_nodes
+        self.output_nodes = output_nodes
+
+        # learning rate
+        self.learning_rate = learning_rate
+
+
 
     # train the neural network
     def train(self):
@@ -15,3 +23,10 @@ class NeuralNetwork:
     # query the neural network
     def query(self):
         pass
+
+# neural network hyperparameters
+input_nodes, hidden_nodes, output_nodes = 3, 3, 3
+alpha = 0.5
+
+# create neural network
+model = NeuralNetwork(input_nodes, hidden_nodes, output_nodes)
