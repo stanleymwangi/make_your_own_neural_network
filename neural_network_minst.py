4c82071c61e92116a7f9419249bfd359a6d53de0
Stanley Mwangi <stanthecoolman@gmail.com>
2018-01-21 22:55:58 +0000

    Add link weight initialisation

--- neural_network_minst.py
+++ neural_network_minst.py
@@ -14,6 +14,11 @@ class NeuralNetwork:
         # learning rate
         self.learning_rate = learning_rate
 
+        # setup weight matrices for links between layers, w_input_hidden and w_hidden_output
+        # weights inside arrays are w_i_j where link is from node i to node j in the next layer
+        # example: w_2_1 is weight moderating signal from node 2 to node 1 in the next layer
+        self.w_input_hidden = np.random.uniform(low=-1.0, high=1.0, size=(self.hidden_nodes, self.input_nodes))
+        self.w_output_hidden = np.random.uniform(low=-1.0, high=1.0, size=(self.hidden_nodes, self.input_nodes))
 
 
     # train the neural network
